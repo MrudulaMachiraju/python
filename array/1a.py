9abfbda63a0817fe96416c946ba9aa3abe4d33db
mrudula <machirajumrudula39@gmail.com>
2024-10-05 19:10:39 +1000

    adding create a array,access,remove,append as 5th commit

---
+++ array/1a.py
@@ -0,0 +1,12 @@
+names=['rinku','tharun','jinka']
+names[0]='finky'
+print(names)
+print(len(names))
+for x in names:
+  print(x)
+names.append('kako')
+print(names)
+names.pop(2)
+print(names)
+names.remove('tharun')
+print(names)
