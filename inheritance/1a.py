1b0210055572784844f703c85b00a7ced8c5d359
mrudula <machirajumrudula39@gmail.com>
2024-10-07 20:20:34 +1100

    adding 6th commit inheritance

---
+++ inheritance/1a.py
@@ -0,0 +1,48 @@
+#create parent class
+
+class Person:
+  def __init__(self, fname, lname):
+    self.firstname = fname
+    self.lastname = lname
+
+  def printname(self):
+    print(self.firstname, self.lastname)
+
+#Use the Person class to create an object, and then execute the printname method:
+
+x = Person("John", "Doe")
+x.printname()
+#create child class
+class Student(Person):
+  pass
+x = Student("Mike", "Olsen")
+x.printname()
+
+#The __init__() function is called automatically every time the class is being used to create a new object.
+class Student(Person):
+  def __init__(self, fname, lname, year):
+    super().__init__(fname, lname)
+    self.graduationyear = year
+
+x = Student("Mike", "Olsen", 2019)
+
+
+#Add a method called welcome to the Student class:
+class Person:
+  def __init__(self, fname, lname):
+    self.firstname = fname
+    self.lastname = lname
+
+  def printname(self):
+    print(self.firstname, self.lastname)
+
+class Student(Person):
+  def __init__(self, fname, lname, year):
+    super().__init__(fname, lname)
+    self.graduationyear = year
+
+  def welcome(self):
+    print("Welcome", self.firstname, self.lastname, "to the class of", self.graduationyear)
+
+x = Student("Mike", "Olsen", 2024)
+x.welcome()
