2604d7cfb85236c2432984e5d78b3d723492bae4
mrudula <machirajumrudula39@gmail.com>
2024-10-08 16:18:56 +1100

    adding polymorphism as my 8th commit

---
+++ polymorphism/1a.py
@@ -0,0 +1,19 @@
+#The word "polymorphism" means "many forms", and in programming 
+# it refers to methods/functions/operators with the same name that can be executed on many objects or classes.
+thisdict = {
+  "brand": "Ford",                         #For strings len() returns the number of characters:
+  "model": "Mustang",                    #For tuples len() returns the number of items in the tuple:
+  "year": 1964                             #For dictionaries len() returns the number of key/value pairs in the dictionary:
+}
+
+print(len(thisdict))
+
+
+
+x = "Hello World!"
+
+print(len(x))
+
+mytuple = ("apple", "banana", "cherry")
+
+print(len(mytuple))
