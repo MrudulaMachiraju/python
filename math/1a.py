0a1246c2e1a31e68b0459c3d4f73b04d3d77d5cc
mrudula <machirajumrudula39@gmail.com>
2024-10-09 22:27:46 +1100

    adding math functions as 12th commit

---
+++ math/1a.py
@@ -0,0 +1,28 @@
+#The min() and max() functions can be used to find the lowest or highest value in an iterable:
+x = min(5, 10, 25)
+y = max(5, 10, 25)
+
+print(x)
+print(y)
+#The abs() function returns the absolute (positive) value of the specified number:
+x = abs(-79.98)
+
+print(x)
+#The pow(x, y) function returns the value of x to the power of y (xy).
+
+x = pow(4, 3)
+
+print(x)
+import math
+
+x = math.sqrt(64)
+
+print(x)
+#ceil and floor methods
+import math
+
+x = math.ceil(1.4)
+y = math.floor(1.4)
+
+print(x) 
+print(y) 
