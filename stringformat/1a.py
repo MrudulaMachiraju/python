3a8343be273a408be2b5022e48db80e17ade06cb
mrudula <machirajumrudula39@gmail.com>
2024-10-11 18:03:57 +1100

    adding strindformat as 15th commit

---
+++ stringformat/1a.py
@@ -0,0 +1,10 @@
+txt = f"The price is 49 dollars"
+print(txt)
+price = 59
+txt = f"The price is {price} dollars"
+print(txt)
+
+price = 59
+tax = 0.25
+txt = f"The price is {price + (price * tax)} dollars"
+print(txt)
